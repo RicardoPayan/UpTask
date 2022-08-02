399fb24f66f04d10bf92c75fe5133f4d2d2fe83e
RicardoPayan <a218206581@unison.mx>
2022-08-02 12:42:44 -0700

    Trabajando en la ventana modal

---
+++ src/js/tareas.js
@@ -0,0 +1,32 @@
+(function (){
+    //Boton para mostrar el modal de agregar tarea
+    const nuevaTareaBtn = document.querySelector('#agregar-tarea');
+    nuevaTareaBtn.addEventListener('click',mostrarFormulario);
+
+
+    function mostrarFormulario(){
+        const modal = document.createElement('div');
+        modal.classList.add('modal');
+
+        modal.innerHTML = `
+            <form class="formulario nueva-tarea">
+                <legend>Añade una nueva tarea</legend>
+                <div class="campo">
+                    <label>Tarea</label>
+                    <input type="text" name="tarea" placeholder="Añadir tarea al proyecto actual" id="tarea">
+                </div>
+                <div class="opciones">
+                    <input type="submit" class="submit-nueva-tarea" value="Añadir Tarea"/>
+                    <button type="button" class="cerrar-modal">Cancelar</button>
+                </div>
+            </form>
+        `;
+
+        setTimeout(()=>{
+            const formulario = document.querySelector('.formulario');
+            formulario.classList.add('animar');
+        },100)
+
+        document.querySelector('body').appendChild(modal);
+    }
+})();
