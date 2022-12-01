c6a0d7489e1dfb7248929b6113da7b97c8791001
RicardoPayan <a218206581@unison.mx>
2022-12-01 12:04:10 -0700

    Trabajando en los detalles de la seccion de tareas

--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -3,7 +3,11 @@
         <div class="contenedor-nueva-tarea">
             <button type="button" class="agregar-tarea" id="agregar-tarea">&#43;Nueva Tarea</button>
         </div>
+
+        <ul id="listado-tareas" class="listado-tareas"></ul>
     </div>
+
+
 <?php include_once __DIR__.'/footer-dashboard.php';?>
 
 <?php
